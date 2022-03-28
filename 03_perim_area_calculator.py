bca89827e7e1f7a0f81469021a608672b66335ec
JackPringle <101750172+JackPringle@users.noreply.github.com>
2022-03-29 12:19:42 +1300

    Created file and added code

---
+++ 03_perim_area_calculator.py
@@ -0,0 +1,23 @@
+# functions go here
+
+# checks input is a number more than zero
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = "Please enter a number that is more than zero"
+
+        try:
+
+            # ask user to enter a number 
+            response = float(input("Enter a number: "))
+
+            # checks number is more than zero
+            if response > 0:
+                valid = True
+
+            # outputs error if input 
+
+
+
+
